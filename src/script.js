a67a32fde53e7916096628dff00d5ef381686935
catarinapfcramos <79986882+catarinapfcramos@users.noreply.github.com>
2021-03-03 14:53:17 +0000

    Updated HTMl CSS and JS files

---
+++ src/script.js
@@ -0,0 +1,87 @@
+function formatDate(date) {
+  let weekDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+  ];
+  let currentDay = weekDays[date.getDay()];
+  let currentHour = date.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  return `${currentDay} ${currentHour}:${currentMinutes}`;
+}
+
+function showWeather(response) {
+  console.log(response);
+  document.querySelector("#currentTemperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#max-temp").innerHTML = Math.round(response.data.main.temp_max);
+  document.querySelector("#min-temp").innerHTML = Math.round(response.data.main.temp_min);
+  document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
+  document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#currentCity").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML = response.data.weather[0].main;
+ }
+
+ function searchCity(city) {
+    let apiKey = "4b590c33d87dbad37bb78d97de248093";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(`${apiUrl}`).then(showWeather);
+ }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-bar").value;
+  searchCity(city);
+}
+
+function changeToCelsius (event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#currentTemperature");
+  currentTemperature.innerHTML = 19;
+}
+function changeToFahrenheit (event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#currentTemperature");
+  currentTemperature.innerHTML = 66;
+}
+
+function showPositionWeather(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "4b590c33d87dbad37bb78d97de248093";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}`).then(showWeather);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPositionWeather)
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#degrees");
+celsiusLink.addEventListener("click", changeToCelsius);
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", changeToFahrenheit);
+
+let currentPositionButton = document.querySelector("#current-button");
+currentPositionButton.addEventListener("click", getCurrentPosition);
+
+searchCity("Porto");
